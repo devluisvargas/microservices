55aaa60fd9182b6d623aa0b4959dea9779bbead4
Luis Vargas <luisbyronvargaspenafiel@gmail.com>
2022-10-09 22:05:57 -0500

    eureka

--- cars/cars/src/main/java/com/devluis/microservices/cars/CarsApplication.java
+++ cars/cars/src/main/java/com/devluis/microservices/cars/CarsApplication.java
@@ -2,8 +2,10 @@ package com.devluis.microservices.cars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CarsApplication {
 
     public static void main(String[] args) {
